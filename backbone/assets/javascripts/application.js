3974f9de045c28d01e7b14768c40772174bd8320
Iván Lòpez <ivlo@MacBook-Pro-de-Ivan-3.local>
2013-11-25 14:41:43 +0100

    labs

---
+++ backbone/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+MyApp = new Backbone.Marionette.Application();
+
+MyApp.addRegions({
+  mainRegion: "#content"
+});
+
+AngryCat = Backbone.Model.extend({});
+
+AngryCats = Backbone.Collection.extend({
+  model: AngryCat
+});
+
+AngryCatView = Backbone.Marionette.ItemView.extend({
+  template: "#angry_cat-template",
+  tagName: 'tr',
+  className: 'angry_cat'
+});
+
+AngryCatsView = Backbone.Marionette.CompositeView.extend({
+  tagName: "table",
+  id: "angry_cats",
+  className: "table-striped table-bordered",
+  template: "#angry_cats-template",
+  itemView: AngryCatView,
+ 
+  appendHtml: function(collectionView, itemView){
+    collectionView.$("tbody").append(itemView.el);
+  }
+});
