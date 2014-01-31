db3ccb1eb9108e71999ee2c1df6cb9ca5f756662
Iván Lòpez <ivlo@MacBook-Pro-de-Ivan-3.local>
2014-01-31 09:16:08 +0100

    ruby and regular expressions

---
+++ ruby/metodos/metodos.rb
@@ -0,0 +1,24 @@
+
+#ARGUMENTOS
+
+def play (instruments ,song)  
+  puts "playing #{song} width my #{instruments}"
+end 
+
+# play "guitar" , "Rape me"
+
+#ARGUMENTOS POR DEFECTO
+
+def play (instrument="with my voice")
+  puts "playing #{instrument}"
+end
+
+#play  "width my ass"
+
+#ARGUMENTOS INFINITOS
+
+def play (instrument, *songs ,band)
+  puts" playing #{songs.join(', ')} width my #{instrument} to #{band} "
+end
+
+play "guitar", "Rape me", "alive", "the man who sold the world, nirvana"
